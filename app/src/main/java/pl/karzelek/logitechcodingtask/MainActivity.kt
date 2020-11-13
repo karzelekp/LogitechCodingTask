31797dc79d91732c6af4f754a4bc5a682e7b09db
Piotr Karzełek <piotr@karzelek.pl>
2020-11-13 08:24:44 +0100

    init

---
+++ app/src/main/java/pl/karzelek/logitechcodingtask/MainActivity.kt
@@ -0,0 +1,18 @@
+package pl.karzelek.logitechcodingtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.karzelek.logitechcodingtask.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/pl/karzelek/logitechcodingtask/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pl.karzelek.logitechcodingtask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
